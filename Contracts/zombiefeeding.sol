87d0d15aa943a79988bddbf860033e64f13d7cab
Heikel <heikel.gonzalez@oracle.com>
2022-02-22 13:08:18 -0300

    Added safeMath library

--- Contracts/zombiefeeding.sol
+++ Contracts/zombiefeeding.sol
@@ -21,7 +21,7 @@ contract ZombieFeeding is ZombieFactory {
     
     KittyInterface kittyContract;
 
-    modifier ownerOf(uint _zombieId) {
+    modifier onlyOwnerOf(uint _zombieId) {
       require(msg.sender == zombieToOwner[_zombieId]);
       _;
     }
@@ -38,7 +38,7 @@ contract ZombieFeeding is ZombieFactory {
       return (_zombie.readyTime <= now);
     }
 
-    function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal ownerOf(_zombieId) {
+    function _feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal onlyOwnerOf(_zombieId) {
         Zombie storage myZombie = zombies[_zombieId];
         require(_isReady(myZombie));
         _targetDna = _targetDna % dnaModulus;
@@ -53,6 +53,6 @@ contract ZombieFeeding is ZombieFactory {
     function feedOnKitty(uint _zombieId, uint _kittyId) public {
       uint kittyDna;
       (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
-      feedAndMultiply(_zombieId, kittyDna, "kitty");
+      _feedAndMultiply(_zombieId, kittyDna, "kitty");
   }
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 Tutorial from https://cryptozombies.io
+
 Solidity contract for a zombie factory.
+
 Javascript backend using Web3.js to interact with the Smart Contract
+
+ETH developer documentation: https://ethereum.org/en/developers/docs/
